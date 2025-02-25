fea8856fa0a0ba024f529416813cfede5d63a3b8
yusezmann <yusezmann@gmail.com>
2025-02-25 19:28:53 +0700

    styling photo

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,27 @@
 import Photo from "@/components/Photo"
+
+// framer motion
 import HeroText from "@/components/HeroText"
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex items-center">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24 gap-12">
-          {/* Text */}
-          <div className="text-center xl:text-left order-2 xl:order-none">
-            <HeroText />
-          </div>
+    <>
+      <div className="h-full">
+        <div className="container mx-auto">
+          <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+            {/* text */}
+            <div className="text-center xl:text-left order-2 xl:order-none">
+              <HeroText />
+            </div>
 
-          {/* Photo */}
-          <div className="order-1 xl:order-none mb-8 md:mb-12 xl:mb-0">
-            <Photo />
+            {/* photo */}
+            <div className="order-1 xl:order-none mb-8 xl:mb-0">
+              <Photo />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- components/Photo.tsx
+++ components/Photo.tsx
@@ -6,43 +6,6 @@ import Image from "next/image"
 const Photo = () => {
   return (
     <div className="relative flex justify-center items-center w-full">
-      {/* Circle Animation */}
-      <motion.div
-        className="absolute flex justify-center items-center"
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 0.5, duration: 0.5, ease: "easeOut" },
-        }}
-      >
-        <motion.svg
-          className="w-[80vw] max-w-[300px] sm:max-w-[400px] lg:max-w-[500px]" // Responsif sesuai ukuran layar
-          viewBox="0 0 500 500"
-          fill="transparent"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <motion.circle
-            cx="250"
-            cy="250"
-            r="240"
-            stroke="#00ff99"
-            strokeWidth="5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            initial={{ strokeDasharray: "24 10 0 0" }}
-            animate={{
-              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
-              rotate: [0, 360],
-            }}
-            transition={{
-              duration: 10,
-              repeat: Infinity,
-              repeatType: "reverse",
-            }}
-          />
-        </motion.svg>
-      </motion.div>
-
       {/* Image */}
       <motion.div
         initial={{ opacity: 0, scale: 0.9 }}
@@ -51,10 +14,10 @@ const Photo = () => {
           scale: 1,
           transition: { delay: 0.8, duration: 0.5, ease: "easeOut" },
         }}
-        className="relative -z-10 w-[80vw] max-w-[280px] sm:max-w-[350px] lg:max-w-[450px] xl:max-w-[650px] xl:left-[80px] top-[28px] xl:top-[45px]" // Responsif
+        className="relative w-[180px] sm:w-[240px] md:w-[300px] lg:w-[350px] xl:w-[400px] top-2 left-1"
       >
         <Image
-          src="/assets/profile.png"
+          src="/assets/profile1.png"
           priority
           quality={100}
           width={500}
@@ -62,6 +25,43 @@ const Photo = () => {
           alt="User Profile"
           className="object-cover rounded-full"
         />
+      </motion.div>
+
+      {/* Circle Animation */}
+      <motion.div
+        className="absolute inset-0 flex justify-center items-center"
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 0.5, duration: 0.5, ease: "easeOut" },
+        }}
+      >
+        <motion.svg
+          className="w-full h-full"
+          viewBox="0 0 500 500"
+          fill="transparent"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="250"
+            cy="250"
+            r="210" // Sesuaikan radius dengan viewBox agar pas
+            stroke="#00ff99"
+            strokeWidth="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "10 20" }}
+            animate={{
+              strokeDasharray: ["15 90 30 30", "16 50 100 70", "4 200 20 20"],
+              rotate: [0, 360],
+            }}
+            transition={{
+              duration: 8,
+              repeat: Infinity,
+              repeatType: "loop",
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   )
